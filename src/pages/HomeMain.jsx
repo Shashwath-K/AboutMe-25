b3814747cf6441482caaa794f32e849310ff64f9
Shashwath-K <shashwathkukkunoor@outlook.com>
2025-10-24 11:33:48 +0530

    Styles have been initialized along with the root

---
+++ src/pages/HomeMain.jsx
@@ -0,0 +1,12 @@
+    import React from 'react';
+
+    function HomeMain() {
+      return (
+        <main>
+          <p>This is the main content area of your simple React homepage.</p>
+          <p>You can add more sections, images, and information here.</p>
+        </main>
+      );
+    }
+
+export default HomeMain;
